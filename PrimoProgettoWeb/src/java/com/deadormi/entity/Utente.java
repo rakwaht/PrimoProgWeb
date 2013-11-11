f7e25a0744bc9801c7242ff767124e96e53b269d
Davide <Davide@10.22.74.193>
2013-11-11 15:27:49 +0100

    init entity

---
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Utente.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.deadormi.entity;
+
+/**
+ *
+ * @author Davide
+ */
+public class Utente {
+    
+}
