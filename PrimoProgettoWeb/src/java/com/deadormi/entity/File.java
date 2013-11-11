4a95a54de47968330f40c430705d02961d329971
Davide <Davide@10.22.74.193>
2013-11-11 15:34:33 +0100

    file e gruppo

--- PrimoProgettoWeb/src/java/com/deadormi/entity/File.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/File.java
@@ -4,10 +4,40 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+import java.sql.Blob;
+
 /**
  *
  * @author Davide
  */
-public class File {
+public class File implements Serializable{
     
+    private Integer id_file;
+    private Blob file;
+    private Integer id_post;
+
+    public Integer getId_file() {
+        return id_file;
+    }
+
+    public void setId_file(Integer id_file) {
+        this.id_file = id_file;
+    }
+
+    public Blob getFile() {
+        return file;
+    }
+
+    public void setFile(Blob file) {
+        this.file = file;
+    }
+
+    public Integer getId_post() {
+        return id_post;
+    }
+
+    public void setId_post(Integer id_post) {
+        this.id_post = id_post;
+    }
 }
--- PrimoProgettoWeb/src/java/com/deadormi/entity/Gruppo.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Gruppo.java
@@ -4,10 +4,59 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+import java.sql.Date;
+
 /**
  *
  * @author Davide
  */
-public class Gruppo {
+public class Gruppo implements Serializable{
     
+     
+    private Integer id_gruppo;
+    private String nome;
+    private Integer id_proprietario;
+    private Date data_creazione;
+    private Boolean gruppo_abilitato;
+
+    public Integer getId_gruppo() {
+        return id_gruppo;
+    }
+
+    public void setId_gruppo(Integer id_gruppo) {
+        this.id_gruppo = id_gruppo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId_proprietario() {
+        return id_proprietario;
+    }
+
+    public void setId_proprietario(Integer id_proprietario) {
+        this.id_proprietario = id_proprietario;
+    }
+
+    public Date getData_creazione() {
+        return data_creazione;
+    }
+
+    public void setData_creazione(Date data_creazione) {
+        this.data_creazione = data_creazione;
+    }
+
+    public Boolean getGruppo_abilitato() {
+        return gruppo_abilitato;
+    }
+
+    public void setGruppo_abilitato(Boolean gruppo_abilitato) {
+        this.gruppo_abilitato = gruppo_abilitato;
+    }
 }
