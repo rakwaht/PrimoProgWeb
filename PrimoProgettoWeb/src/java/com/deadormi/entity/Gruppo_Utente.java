5ce55d9c736f4e683d48c58b4c98743db88d0a57
Timbu <Timbu@10.22.51.142>
2013-11-11 15:35:54 +0100

    -Added Invito.java setter & getter; -Added Gruppo_Utente.java setter & getter;

--- PrimoProgettoWeb/src/java/com/deadormi/entity/Gruppo_Utente.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Gruppo_Utente.java
@@ -4,10 +4,30 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Davide
  */
-public class Gruppo_Utente {
+public class Gruppo_Utente implements Serializable{
+    private Integer id_gruppo;
+    private Integer id_utente;
+
+    public Integer getId_gruppo() {
+        return id_gruppo;
+    }
+
+    public void setId_gruppo(Integer id_gruppo) {
+        this.id_gruppo = id_gruppo;
+    }
+
+    public Integer getId_utente() {
+        return id_utente;
+    }
+
+    public void setId_utente(Integer id_utente) {
+        this.id_utente = id_utente;
+    }
     
 }
--- PrimoProgettoWeb/src/java/com/deadormi/entity/Invito.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Invito.java
@@ -4,10 +4,48 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Davide
  */
-public class Invito {
+public class Invito implements Serializable {
+    private Integer id_invito;
+    private Integer id_invitato;
+    private Integer id_invitante;
+    private Integer id_gruppo;
+
+    public Integer getId_invito() {
+        return id_invito;
+    }
+
+    public void setId_invito(Integer id_invito) {
+        this.id_invito = id_invito;
+    }
+
+    public Integer getId_invitato() {
+        return id_invitato;
+    }
+
+    public void setId_invitato(Integer id_invitato) {
+        this.id_invitato = id_invitato;
+    }
+
+    public Integer getId_invitante() {
+        return id_invitante;
+    }
+
+    public void setId_invitante(Integer id_invitante) {
+        this.id_invitante = id_invitante;
+    }
+
+    public Integer getId_gruppo() {
+        return id_gruppo;
+    }
+
+    public void setId_gruppo(Integer id_gruppo) {
+        this.id_gruppo = id_gruppo;
+    }
     
 }
