3beff34e5aacfd8dbb843378a9e6391cd6694bf7
Francesco Serra <afnecors@gmail.com>
2013-11-11 15:36:22 +0100

    post utente classi

--- PrimoProgettoWeb/src/java/com/deadormi/entity/Post.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Post.java
@@ -4,10 +4,48 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Davide
  */
-public class Post {
+public class Post implements Serializable {
+    private Integer id_post;
+    private Integer id_scrivente;
+    private Integer id_gruppo;
+    private String testo;
+
+    public void setId_post(Integer id_post) {
+        this.id_post = id_post;
+    }
+
+    public void setId_scrivente(Integer id_scrivente) {
+        this.id_scrivente = id_scrivente;
+    }
+
+    public void setId_gruppo(Integer id_gruppo) {
+        this.id_gruppo = id_gruppo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+    public Integer getId_post() {
+        return id_post;
+    }
+
+    public Integer getId_scrivente() {
+        return id_scrivente;
+    }
+
+    public Integer getId_gruppo() {
+        return id_gruppo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
     
 }
--- PrimoProgettoWeb/src/java/com/deadormi/entity/Utente.java
+++ PrimoProgettoWeb/src/java/com/deadormi/entity/Utente.java
@@ -4,10 +4,57 @@
  */
 package com.deadormi.entity;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Davide
  */
-public class Utente {
+public class Utente implements Serializable {
+    private Integer id_utente;
+    private String nome;
+    private String password;
+    private Boolean utente_abilitato;
+    private Integer id_avatar;
+
+    public void setId_utente(Integer id_utente) {
+        this.id_utente = id_utente;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUtente_abilitato(Boolean utente_abilitato) {
+        this.utente_abilitato = utente_abilitato;
+    }
+
+    public void setId_avatar(Integer id_avatar) {
+        this.id_avatar = id_avatar;
+    }
+
+    public Integer getId_utente() {
+        return id_utente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean isUtente_abilitato() {
+        return utente_abilitato;
+    }
+
+    public Integer getId_avatar() {
+        return id_avatar;
+    }
     
 }
