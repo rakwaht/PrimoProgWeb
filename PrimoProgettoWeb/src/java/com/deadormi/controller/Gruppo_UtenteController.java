2d1c2eaa110a387a25afa880686b7ea9486ec93d
Timbu <Timbu@10.22.101.86>
2013-11-13 13:43:48 +0100

    -Pagina mieigruppi completata :-P

--- PrimoProgettoWeb/src/java/com/deadormi/controller/Gruppo_UtenteController.java
+++ PrimoProgettoWeb/src/java/com/deadormi/controller/Gruppo_UtenteController.java
@@ -8,7 +8,9 @@ import com.deadormi.dbmanager.DbManager;
 import com.deadormi.entity.Gruppo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -31,20 +33,37 @@ public class Gruppo_UtenteController {
         } finally {
             stm.close();
         }
-    } 
+    }
 
     public static List<Gruppo> getGruppiByUserId(HttpServletRequest request) throws SQLException {
         DbManager dbmanager = (DbManager) request.getServletContext().getAttribute("dbmanager");
         Connection connection = dbmanager.getConnection();
         HttpSession session = request.getSession();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO ROOT.GRUPPO_UTENTE (id_gruppo,id_utente) VALUES (?,?)");
+        List<Gruppo> miei_gruppi = new ArrayList<Gruppo>();
+        Gruppo gruppo;
+        ResultSet rs;
+        PreparedStatement stm = connection.prepareStatement("SELECT * FROM ROOT.GRUPPO_UTENTE NATURAL JOIN ROOT.GRUPPO WHERE id_utente=? ");
         try {
-            stm.setInt(1, Integer.parseInt(id_gruppo));
-            stm.setInt(2, (Integer) session.getAttribute("user_id"));
-            stm.executeUpdate();
+            stm.setInt(1, (Integer)session.getAttribute("user_id"));
+            rs = stm.executeQuery();
+            try {
+                while (rs.next()) {
+                    gruppo = new Gruppo();
+                    gruppo.setData_creazione(rs.getString("data_creazione"));
+                    gruppo.setId_gruppo(rs.getInt("id_gruppo"));
+                    gruppo.setDescrizione(rs.getString("descrizione"));
+                    gruppo.setGruppo_abilitato(rs.getBoolean("gruppo_abilitato"));
+                    gruppo.setId_proprietario(rs.getInt("id_proprietario"));
+                    gruppo.setNome(rs.getString("nome"));
+                    miei_gruppi.add(gruppo);
+                }
+            } finally {
+                   rs.close();
+            }
         } finally {
             stm.close();
-        }  
+        }
+        return miei_gruppi;
     }
-    
+
 }
--- PrimoProgettoWeb/src/java/com/deadormi/servlet/TuoiGruppiServlet.java
+++ PrimoProgettoWeb/src/java/com/deadormi/servlet/TuoiGruppiServlet.java
@@ -5,11 +5,16 @@
 package com.deadormi.servlet;
 
 import com.deadormi.controller.Gruppo_UtenteController;
+import com.deadormi.controller.UtenteController;
 import com.deadormi.entity.Gruppo;
+import com.deadormi.entity.Utente;
 import com.deadormi.layout.MainLayout;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,9 +27,8 @@ import javax.servlet.http.HttpServletResponse;
 public class TuoiGruppiServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -35,23 +39,45 @@ public class TuoiGruppiServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        List<Gruppo> gruppi = Gruppo_UtenteController.getGruppiByUserId(request);
+        List<Gruppo> gruppi = null;
+        try {
+            gruppi = Gruppo_UtenteController.getGruppiByUserId(request);
+        } catch (SQLException ex) {
+            Logger.getLogger(TuoiGruppiServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
         try {
             /* TODO output your page here. You may use following sample code. */
             MainLayout.printHeader(out);
             out.println("<h1>MIEI GRUPPI: " + request.getContextPath() + "</h1>");
             out.println("<a href='home'>Indietro</a><br />");
-            out.println("<table>");
+            if (gruppi != null && gruppi.size() > 0) {
+                out.println("<table>");
+                for (int i = 0; i < gruppi.size(); i++) {
+                    Gruppo gruppo = gruppi.get(i);
+                    Utente proprietario = null;
+                    try {
+                        proprietario = UtenteController.getUserById(request, gruppo.getId_proprietario());
+                    } catch (SQLException ex) {
+                        Logger.getLogger(TuoiGruppiServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    out.println("<tr>");
+                    out.println("<td>Nome: "+ gruppo.getNome() + " Autore: "+ proprietario.getUsername() +"</td>");
+                    out.println("<td>Descrizione:  "+ gruppo.getDescrizione() +"</td>");
+                    out.println("</tr>");
+                }
+                out.println("</table>");
+            } else {
+                out.println("<p>Nessun gruppo iscritto!</p>");
+            }
             MainLayout.printFooter(out);
-        } finally {            
+        } finally {
             out.close();
         }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -65,8 +91,7 @@ public class TuoiGruppiServlet extends HttpServlet {
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
