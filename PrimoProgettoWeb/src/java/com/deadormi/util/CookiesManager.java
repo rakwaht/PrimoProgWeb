639600c8197d4c313ffc42735c632acf2599420f
Timbu <Timbu@10.22.101.86>
2013-11-14 15:52:43 +0100

    -Martin sei un criminale bastardo

--- PrimoProgettoWeb/src/java/com/deadormi/util/CookiesManager.java
+++ PrimoProgettoWeb/src/java/com/deadormi/util/CookiesManager.java
@@ -18,29 +18,32 @@ public class CookiesManager {
 
     public static void createNewDateCookie(HttpServletRequest request, HttpServletResponse response) {
         Cookie[] cookies = request.getCookies();
-        
         HttpSession session = request.getSession();
         Cookie cookie;
         
         if (cookies == null) {
-            cookie = new Cookie("ultimo_login?" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
-            cookie.setMaxAge(60*60);
-            
+            cookie = new Cookie("ultimo_login" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
+            cookie.setMaxAge(60*60*200);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/PrimoProgettoWeb/");
             response.addCookie(cookie);
         } else {
             for (int i = 0; i < cookies.length; i++) {
                 cookie = cookies[i];
-                System.out.println(cookies.length + cookie.getName() + "ultimo_login?" + session.getAttribute("user_id").toString());
-                if (cookie.getName().equals("ultimo_login?"+session.getAttribute("user_id").toString())) {
+                System.out.println(cookies.length + cookie.getName() + "ultimo_login" + session.getAttribute("user_id").toString());
+                if (cookie.getName().equals("ultimo_login"+session.getAttribute("user_id").toString())) {
                     CookiesManager.createOldDateCookie(cookie.getValue(), response, request);
                     cookie.setMaxAge(0);
-                    cookie = new Cookie("ultimo_login?" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
+                    cookie = new Cookie("ultimo_login" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
+                    cookie.setPath("/PrimoProgettoWeb/");
                     cookie.setMaxAge(604800);
                     response.addCookie(cookie);
                 } else {
                     System.out.println("ciao");
-                    cookie = new Cookie("ultimo_login?" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
+                    cookie = new Cookie("ultimo_login" + session.getAttribute("user_id"), CurrentDate.getCurrentDate());
                     cookie.setMaxAge(604800);
+                     cookie.setPath("/PrimoProgettoWeb/");
+                     cookie.setHttpOnly(true);
                     response.addCookie(cookie);
                 }
             }
@@ -49,7 +52,7 @@ public class CookiesManager {
 
     public static void createOldDateCookie(String date, HttpServletResponse response, HttpServletRequest request) {
         HttpSession session = request.getSession();
-        Cookie cookie = new Cookie("old_cookie?" + session.getAttribute("user_id"), date);
+        Cookie cookie = new Cookie("old_cookie" + session.getAttribute("user_id"), date);
         cookie.setMaxAge(604800);
         response.addCookie(cookie);
     }
@@ -61,7 +64,7 @@ public class CookiesManager {
         String res = null;
         for (int i = 0; i < cookies.length; i++) {
             cookie = cookies[i];
-            if (cookie.getName().equals("old_cookie?" + session.getAttribute("user_id"))) {
+            if (cookie.getName().equals("old_cookie" + session.getAttribute("user_id"))) {
                 res = cookie.getValue();
             }
         }
