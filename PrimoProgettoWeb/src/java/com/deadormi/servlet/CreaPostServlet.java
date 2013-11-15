461d2940314f281f1c79e2e28ca589efdc1bfd4c
Davide <Davide@10.22.74.193>
2013-11-15 09:13:12 +0100

    creaPost tentativo #2.1

--- PrimoProgettoWeb/src/java/com/deadormi/servlet/CreaPostServlet.java
+++ PrimoProgettoWeb/src/java/com/deadormi/servlet/CreaPostServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 /**
  *
