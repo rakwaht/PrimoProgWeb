c95362c094bc5a8bfe4c1f2d36fa0728160c7c88
Timbu <Timbu@10.22.185.125>
2013-11-19 14:01:07 +0100

    Parsing fatto.

--- PrimoProgettoWeb/src/java/com/deadormi/layout/MainLayout.java
+++ PrimoProgettoWeb/src/java/com/deadormi/layout/MainLayout.java
@@ -22,7 +22,7 @@ public class MainLayout {
             out.println("<link rel='stylesheet' type='text/css' href='/PrimoProgettoWeb/res/css/application.css' />");
             out.println("<link rel='stylesheet' type='text/css' href='/PrimoProgettoWeb/res/css/semantic.min.css' />");
             out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/jquery_v2.0.3.js'></script>");
-            out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/semantic.min.js'></script>");         
+            out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/semantic.js'></script>");         
             out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/application.js'></script>");
             out.println("<title>Servlet HomeServlet cul</title>");
             out.println("<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>");
