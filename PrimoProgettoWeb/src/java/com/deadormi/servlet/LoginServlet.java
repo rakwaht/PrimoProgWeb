1eb2d6d7fc9029603adbcacb60d290e0d494c905
Timbu <Timbu@10.22.196.207>
2013-11-20 10:47:30 +0100

    Start Frontend

--- PrimoProgettoWeb/src/java/com/deadormi/servlet/LoginServlet.java
+++ PrimoProgettoWeb/src/java/com/deadormi/servlet/LoginServlet.java
@@ -26,9 +26,8 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -47,18 +46,45 @@ public class LoginServlet extends HttpServlet {
             out.println("<title>Servlet HomeServlet</title>");
             out.println("<link rel='stylesheet' type='text/css' href='/PrimoProgettoWeb/res/css/application.css' />");
             out.println("<link rel='stylesheet' type='text/css' href='/PrimoProgettoWeb/res/css/semantic.css' />");
+            out.println("<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>");
             out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/jquery_v2.0.3.js'></script>");
-            out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/semantic.js'></script>");         
+            out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/semantic.js'></script>");
             out.println("<script type='text/javascript' src='/PrimoProgettoWeb/res/js/application.js'></script>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("<form action=\"login\" method=\"POST\">");
-            out.println("Username:<input type=\"text\" name=\"username\" placeholder=\"Username\"/>");
-            out.println("<br />");
-            out.println("Password:<input type=\"password\" name=\"password\" placeholder=\"Password\" />");
-            out.println("<input type=\"submit\" value=\"Login\" />");
+
+            out.println("<h1 class='center ui header'>SiteName</h1>");
+            out.println("<div class='ui grid'>");
+            out.println("<div class='five wide column'></div>");
+
+            out.println("<div class='six wide column'>");
+            out.println("<form class='ui fluid form segment' method='POST'");
+            out.println("<div class='three fields'>");
+            out.println("<h2 class='center ui header' id='login-title'><i class='circular inverted blue icon sign in'></i>Login</h2>");
+            out.println("<div class='ui divider'></div>");
+            out.println("<div class='field'>");
+            out.println("<div class='ui blue ribbon label'>Username</div>");
+            out.println("<div class='ui left icon input login-input'>");
+            out.println("<input placeholder='Username' type='text' name='username'/>");
+            out.println("<i class='icon user'></i>");
+            out.println("</div>");
+            out.println("</div>");
+            out.println("<div class='field'>");
+            out.println("<div class='ui blue ribbon label'>Password</div>");
+            out.println("<div class='ui left icon input login-input'>");
+            out.println("<i class='icon key'></i>");
+            out.println("<input placeholder='Password' type='password' name='password'/>");
+            out.println("</div>");
+            out.println("</div>");
+            out.println("<div class='field'>");
+            out.println("<button type='submit' class='ui blue submit fluid button'>Invia</button>");
+            out.println("</div>");
             out.println("</form>");
+            out.println("</div>");
+
+            out.println("<div class='five wide column'></div>");
+
+            out.println("</div>");
             MainLayout.printFooter(out);
         } finally {
             out.close();
@@ -67,8 +93,7 @@ public class LoginServlet extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -82,8 +107,7 @@ public class LoginServlet extends HttpServlet {
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
