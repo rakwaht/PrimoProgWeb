e912d4a112549f65e69c00d7f27351ec1663823f
Timbu <Timbu@10.22.220.219>
2013-11-21 10:26:28 +0100

    FrontEnd: Pagina Home 1.1

--- PrimoProgettoWeb/web/res/js/application.js
+++ PrimoProgettoWeb/web/res/js/application.js
@@ -4,12 +4,6 @@
  */
 
 
-//test presenza di jquery
-function myFunction()
-{
-    $("#test").html("Hello jQuery");
-}
-$(document).ready(myFunction);
 
 
 //funzione per linkare i file alla creazione dei post
@@ -23,6 +17,8 @@ function linka_selezionati() {
 
 
 //aggiungo dinamicamente input file alla crea post
-function add_upload_file(){
+function add_upload_file() {
     $('form input:file').last().after($('<br /><input type="file" name="file" class="file" onchange="add_upload_file();" />'));
-};
+}
+;
+
