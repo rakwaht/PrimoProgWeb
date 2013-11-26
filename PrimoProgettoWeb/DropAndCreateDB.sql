979e07d2372c14b3c99c1a109c4a13223a3775e0
Timbu <Timbu@10.22.11.4>
2013-11-26 17:31:16 +0100

    UPDATED DB QUERY !

--- PrimoProgettoWeb/DropAndCreateDB.sql
+++ PrimoProgettoWeb/DropAndCreateDB.sql
@@ -67,13 +67,119 @@ FOREIGN KEY (id_invitante) REFERENCES utente(id_utente),
 FOREIGN KEY (id_gruppo) REFERENCES gruppo(id_gruppo)
 );
 
+
 INSERT INTO utente (username,password)
-VALUES ('pippo','pippa');
+VALUES ('federico','federico');
 INSERT INTO utente (username,password)
-VALUES ('gino','perna');
+VALUES ('davide','davide');
 INSERT INTO utente (username,password)
-VALUES ('giovanni','rossi');
+VALUES ('francesco','francesco');
 INSERT INTO utente (username,password)
-VALUES ('susanna','panna');
+VALUES ('susanna','susanna');
 INSERT INTO utente (username,password)
-VALUES ('giovanna','panna');
+VALUES ('giovanna','giovanna');
+
+
+INSERT INTO gruppo (nome,id_proprietario,data_creazione,descrizione)
+VALUES ('[Programmazione per il Web]',1,'2013-01-16 alle 14:50:11','In questo gruppo potrete chiedere informazioni ed aiuto per questo corso.');
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (1,1);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (1,2);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (1,3);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (1,4);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (1,5);
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(2,'2013-11-15 alle 19:20:59',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(3,'2013-11-16 alle 19:20:50',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-17 alle 19:12:51',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-19 alle 19:11:10',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(2,'2013-11-15 alle 19:10:44',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(4,'2013-11-11 alle 19:00:60',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(3,'2013-11-27 alle 19:00:12',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-27 alle 18:20:56',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-17 alle 18:14:23',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(2,'2013-10-19 alle 18:10:52',1,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+
+
+
+
+
+
+INSERT INTO gruppo (nome,id_proprietario,data_creazione,descrizione)
+VALUES ('[Linguaggi formali e compilatori]',3,'2013-10-11 alle 19:20:59','In questo gruppo potrete chiedere informazioni ed aiuto per questo corso.');
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (2,1);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (2,3);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (2,5);
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-15 alle 19:20:59',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(3,'2013-11-16 alle 19:20:50',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-17 alle 19:12:51',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(3,'2013-11-19 alle 19:11:10',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-15 alle 19:10:44',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-11 alle 19:00:60',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-27 alle 19:00:12',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-27 alle 18:20:56',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(3,'2013-11-17 alle 18:14:23',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-10-19 alle 18:10:52',2,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+
+
+
+
+
+INSERT INTO gruppo (nome,id_proprietario,data_creazione,descrizione)
+VALUES ('[Human Computer Interaction]',5,'1969-08-09 alle 23:33:10','In questo gruppo potrete chiedere informazioni ed aiuto per questo corso.');
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (3,1);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (3,2);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (3,4);
+INSERT INTO gruppo_utente (id_gruppo,id_utente)
+VALUES (3,5);
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-15 alle 19:20:59',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(2,'2013-11-16 alle 19:20:50',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-17 alle 19:12:51',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(4,'2013-11-19 alle 19:11:10',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-11-15 alle 19:10:44',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-11 alle 19:00:60',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(4,'2013-11-27 alle 19:00:12',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(5,'2013-11-27 alle 18:20:56',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(2,'2013-11-17 alle 18:14:23',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+INSERT INTO post(id_scrivente,data_creazione,id_gruppo,testo)
+VALUES(1,'2013-10-19 alle 18:10:52',3,'Bacon ipsum dolor sit amet shankle kielbasa doner, short loin ut venison salami fatback fugiat nostrud sunt ad meatloaf. Corned beef pork brisket, kevin venison shankle non cillum et swine. Turducken aute cillum salami est incididunt shoulder sausage consectetur dolore corned beef. Biltong sirloin quis, veniam in short ribs flank fatback spare ribs. Chicken frankfurter dolore, irure rump labore tenderloin reprehenderit adipisicing.');
+
+
